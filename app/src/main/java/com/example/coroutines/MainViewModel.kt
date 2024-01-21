3791591364ab1ba99c29374e0bd1c987e311c7b1
zim <renegadez@tut.by>
2024-01-21 10:24:24 +0300

    Async and Launch

--- app/src/main/java/com/example/coroutines/MainViewModel.kt
+++ app/src/main/java/com/example/coroutines/MainViewModel.kt
@@ -6,13 +6,15 @@ import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
-    private val parentJob = Job()
+    private val parentJob = SupervisorJob()
     private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
         Log.d(LOG_TAG, "Exception caught: $throwable")
     }
@@ -27,10 +29,13 @@ class MainViewModel : ViewModel() {
             delay(2000)
             Log.d(LOG_TAG, "second coroutine finished")
         }
-        val childJob3 = coroutineScope.launch {
+        val childJob3 = coroutineScope.async {
             delay(1000)
             error()
             Log.d(LOG_TAG, "third coroutine finished")
+        }
+        coroutineScope.launch {
+            childJob3.await()
         }
     }
 
